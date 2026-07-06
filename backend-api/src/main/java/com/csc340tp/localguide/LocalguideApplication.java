39bca525f6666b0e8793734c416ff271d79c209c
Pedro Magalahaes <p_magalhaes@uncg.edu>
2026-07-06 10:29:00 -0400

    added scaffolding files for API due to class

---
+++ backend-api/src/main/java/com/csc340tp/localguide/LocalguideApplication.java
@@ -0,0 +1,13 @@
+package com.csc340tp.localguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalguideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalguideApplication.class, args);
+	}
+
+}
---
+++ backend-api/src/test/java/com/csc340tp/localguide/LocalguideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csc340tp.localguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalguideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
